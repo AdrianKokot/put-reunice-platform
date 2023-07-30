77dbba00623e9e5b58c9b1a840a5cc308541ab9f
Adrian Kokot <kokotadriann@gmail.com>
2023-07-30 16:16:32 +0200

    lint: simplify ts, jest and eslint configs

--- frontend/modules/admin/README.md
+++
@@ -1,7 +0,0 @@
-# modules-admin
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test modules-admin` to execute the unit tests.
--- frontend/modules/admin/src/lib/user/user.module.ts
+++ frontend/modules/admin/src/lib/user/user.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { UserListComponent } from './feature/user-list/user-list.component';
 
 @NgModule({
   imports: [CommonModule, UserRoutingModule],
