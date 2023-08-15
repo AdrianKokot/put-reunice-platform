e901688c4ee3dbd5b8a8d6e1921c948a70e0af59
BartoszDolata <bartosz.dolata@student.put.poznan.pl>
2023-08-15 22:52:31 +0200

    Resolve issue with email service

--- backend/src/main/java/com/example/cms/email/EmailSendingService.java
+++ backend/src/main/java/com/example/cms/email/EmailSendingService.java
@@ -1,6 +1,7 @@
 package com.example.cms.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,10 +12,12 @@ public class EmailSendingService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${SMTP_USERNAME}")
+    private String sender;
     public void sendEmail(String receiver, String subject, String body){
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("test@mailpit");
+        message.setFrom(sender);
         message.setTo(receiver);
         message.setSubject(subject);
         message.setText(body);
