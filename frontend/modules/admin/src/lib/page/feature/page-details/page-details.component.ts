bf7d6bd69d37d9e9b71e0361a2bdcddbc92fe389
Adrian Kokot <kokotadriann@gmail.com>
2023-08-17 10:35:53 +0200

    feat: add page details page

--- frontend/modules/admin/src/lib/page/feature/page-details/page-details.component.ts
+++ frontend/modules/admin/src/lib/page/feature/page-details/page-details.component.ts
@@ -1,12 +1,49 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { TuiButtonModule, TuiLabelModule } from '@taiga-ui/core';
+import { TuiHandler, TuiLetModule } from '@taiga-ui/cdk';
+import { TuiEditorSocketModule } from '@tinkoff/tui-editor';
+import { resourceFromRoute } from '@reunice/modules/shared/util';
+import { Page, PageService } from '@reunice/modules/shared/data-access';
+import { RouterLink } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiIslandModule, TuiTreeModule } from '@taiga-ui/kit';
+import { filter, shareReplay, startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'reunice-page-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    TuiLabelModule,
+    TuiLetModule,
+    TuiButtonModule,
+    TuiEditorSocketModule,
+    RouterLink,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiIslandModule,
+    TuiTreeModule,
+  ],
   templateUrl: './page-details.component.html',
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageDetailsComponent {}
+export class PageDetailsComponent {
+  private readonly _service = inject(PageService);
+  readonly item$ = resourceFromRoute(this._service);
+
+  readonly pagesTree$ = this.item$.pipe(
+    filter((page): page is Page => page !== null),
+    switchMap((page) =>
+      this._service
+        .getUniversityHierarchy(page.university.id)
+        .pipe(startWith(null))
+    ),
+    shareReplay()
+  );
+
+  readonly pagesTreeHandler: TuiHandler<Page, readonly Page[]> = (item) =>
+    item?.children ?? [];
+}
