87aa45ac00b3ed679ca96286659d96ba5391bc98
Adrian Kokot <kokotadriann@gmail.com>
2023-08-17 11:05:56 +0200

    feat: add user details page

--- frontend/modules/admin/src/lib/user/feature/user-details/user-details.component.ts
+++ frontend/modules/admin/src/lib/user/feature/user-details/user-details.component.ts
@@ -1,12 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { TuiButtonModule, TuiLabelModule } from '@taiga-ui/core';
+import { TuiLetModule } from '@taiga-ui/cdk';
+import {
+  NgForTrackByIdDirective,
+  resourceFromRoute,
+} from '@reunice/modules/shared/util';
+import { UserService } from '@reunice/modules/shared/data-access';
+import { RouterLink } from '@angular/router';
+import { TuiTagModule } from '@taiga-ui/kit';
+import { UserDirective } from '@reunice/modules/shared/security';
 
 @Component({
   selector: 'reunice-user-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    TuiLabelModule,
+    TuiLetModule,
+    TuiButtonModule,
+    RouterLink,
+    NgForTrackByIdDirective,
+    TuiTagModule,
+    UserDirective,
+  ],
   templateUrl: './user-details.component.html',
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserDetailsComponent {}
+export class UserDetailsComponent {
+  readonly item$ = resourceFromRoute(inject(UserService));
+}
