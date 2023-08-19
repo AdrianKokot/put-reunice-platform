6f8a8a51150ee5f28934f26021412e35a6e28521
Adrian Kokot <kokotadriann@gmail.com>
2023-08-19 11:29:15 +0200

    feat: add university details page

--- frontend/modules/admin/src/lib/university/feature/university-details/university-details.component.ts
+++ frontend/modules/admin/src/lib/university/feature/university-details/university-details.component.ts
@@ -1,12 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {resourceFromRoute} from "@reunice/modules/shared/util";
+import {UniversityService} from "@reunice/modules/shared/data-access";
+import {TuiButtonModule, TuiLabelModule} from "@taiga-ui/core";
+import {TuiLetModule} from "@taiga-ui/cdk";
+import {TranslateModule} from "@ngx-translate/core";
+import {TuiEditorSocketModule} from "@tinkoff/tui-editor";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'reunice-university-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TuiLabelModule,
+    TuiLetModule,
+    TranslateModule,
+    TuiEditorSocketModule,
+    RouterLink,
+    TuiButtonModule,
+  ],
   templateUrl: './university-details.component.html',
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UniversityDetailsComponent {}
+export class UniversityDetailsComponent {
+  readonly item$ = resourceFromRoute(inject(UniversityService));
+}
