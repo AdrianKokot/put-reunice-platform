6be4d1b2ebebec86610f8e9c7dffe7b6058547f2
mudziok <mudziok@gmail.com>
2023-08-23 20:18:08 +0200

    Implement closing sidebar on route change

--- frontend/modules/shared/ui/src/lib/layout/sidebar/sidebar.component.ts
+++ frontend/modules/shared/ui/src/lib/layout/sidebar/sidebar.component.ts
@@ -1,13 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
-import { TuiActiveZoneModule } from '@taiga-ui/cdk';
+import { TuiActiveZoneDirective, TuiActiveZoneModule } from '@taiga-ui/cdk';
 import { TuiButtonModule } from '@taiga-ui/core';
 import { PolymorpheusModule } from '@tinkoff/ng-polymorpheus';
+import {
+  Observable,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  merge,
+} from 'rxjs';
 
 @Component({
   selector: 'reunice-sidebar',
   standalone: true,
   imports: [
+    CommonModule,
     TuiButtonModule,
     TuiSidebarModule,
     TuiActiveZoneModule,
@@ -16,10 +33,24 @@ import { PolymorpheusModule } from '@tinkoff/ng-polymorpheus';
   templateUrl: './sidebar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent {
-  open = false;
+export class SidebarComponent implements AfterViewInit {
+  @ViewChild(TuiActiveZoneDirective)
+  private readonly _button!: TuiActiveZoneDirective;
 
-  toggle(open: boolean) {
-    this.open = open;
+  open$!: Observable<boolean>;
+
+  private readonly _router = inject(Router);
+
+  ngAfterViewInit() {
+    this.open$ = merge(
+      this._button.tuiActiveZoneChange,
+      fromEvent(this._button['el'].nativeElement, 'click').pipe(
+        map(() => true)
+      ),
+      this._router.events.pipe(
+        filter((e) => e instanceof NavigationEnd),
+        map(() => false)
+      )
+    ).pipe(distinctUntilChanged());
   }
 }
