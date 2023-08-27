9d30f10988421562e6228b372aad6f975eb63e8e
Adrian Kokot <kokotadriann@gmail.com>
2023-08-27 18:24:56 +0200

    revert: PageSpecification changes
    Moved changes to #51

--- backend/src/main/java/com/example/cms/page/PageSpecification.java
+++ backend/src/main/java/com/example/cms/page/PageSpecification.java
@@ -1,15 +1,22 @@
 package com.example.cms.page;
 
 import com.example.cms.SearchCriteria;
+import com.example.cms.security.LoggedUser;
+import com.example.cms.security.Role;
+import com.example.cms.security.SecurityService;
+import com.example.cms.university.University;
 import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.Collection;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.Boolean.parseBoolean;
 import static java.lang.Integer.parseInt;
 
 @AllArgsConstructor
@@ -22,52 +29,49 @@ public class PageSpecification implements Specification<Page> {
 
     @Override
     public Predicate toPredicate(Root<Page> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-//        Predicate predicates = criteriaBuilder.and(criteriaBuilder.or(
-//                criteriaBuilder.and(
-//                        criteriaBuilder.equal(root.get("hidden"), false),
-//                        criteriaBuilder.equal(root.get("university").get("hidden"), false)
-//                )));
+        Predicate predicates = criteriaBuilder.and(criteriaBuilder.or(
+                criteriaBuilder.and(
+                        criteriaBuilder.equal(root.get("hidden"), false),
+                        criteriaBuilder.equal(root.get("university").get("hidden"), false)
+                )));
 
-//        if (this.role != null) {
-//            predicates = criteriaBuilder.and(predicates,
-//                    criteriaBuilder.equal(criteriaBuilder.literal(role), "ADMIN"));
-//
-//            if (this.creator != null) {
-//                predicates = criteriaBuilder.and(predicates,
-//                        criteriaBuilder.and(
-//                                criteriaBuilder.equal(criteriaBuilder.literal(role), "MODERATOR"),
-//                                root.get("university").get("id").in(universities)));
-//            }
-//
-//            if (this.universities != null) {
-//                predicates = criteriaBuilder.and(predicates,
-//                        criteriaBuilder.and(
-//                                criteriaBuilder.equal(criteriaBuilder.literal(role), "USER"),
-//                                criteriaBuilder.equal(root.get("creator").get("id"), creator)));
-//            }
-//        }
+        if (this.role != null) {
+            predicates = criteriaBuilder.and(predicates,
+                    criteriaBuilder.equal(criteriaBuilder.literal(role), "ADMIN"));
+
+            if (this.creator != null) {
+                predicates = criteriaBuilder.and(predicates,
+                        criteriaBuilder.and(
+                                criteriaBuilder.equal(criteriaBuilder.literal(role), "MODERATOR"),
+                                root.get("university").get("id").in(universities)));
+            }
+
+            if (this.universities != null) {
+                predicates = criteriaBuilder.and(predicates,
+                        criteriaBuilder.and(
+                                criteriaBuilder.equal(criteriaBuilder.literal(role), "USER"),
+                                criteriaBuilder.equal(root.get("creator").get("id"), creator)));
+            }
+        }
 
         if (criteria.getOperation().equalsIgnoreCase("ct")) {
             if (root.get(criteria.getKey()).getJavaType() == String.class) {
                 return criteriaBuilder.and(
                         criteriaBuilder.like(
-                                criteriaBuilder.lower(root.get(criteria.getKey())), "%" + criteria.getValue().toString().toLowerCase() + "%"));
+                                root.get(criteria.getKey()), "%" + criteria.getValue() + "%"),
+                        predicates);
             }
         } else if (criteria.getOperation().equalsIgnoreCase("eq")) {
-            if (criteria.getKey().equalsIgnoreCase("university")) {
-                return criteriaBuilder.and(
-                        criteriaBuilder.equal(root.get("university").get("id"), criteria.getValue()));
-            } else if (criteria.getKey().equalsIgnoreCase("creator")) {
-                return criteriaBuilder.and(
-                        criteriaBuilder.equal(root.get("creator").get("id"), criteria.getValue()));
-            } else if (root.get(criteria.getKey()).getJavaType() == String.class) {
+            if (root.get(criteria.getKey()).getJavaType() == String.class) {
                 return criteriaBuilder.and(
                         criteriaBuilder.equal(
-                                root.<String>get(criteria.getKey()), criteria.getValue()));
-            } else if (root.get(criteria.getKey()).getJavaType() == boolean.class || root.get(criteria.getKey()).getJavaType() == Boolean.class) {
+                                root.<String>get(criteria.getKey()), criteria.getValue()),
+                        predicates);
+            } else if (root.get(criteria.getKey()).getJavaType() == boolean.class) {
                 return criteriaBuilder.and(
                         criteriaBuilder.equal(
-                                root.<String>get(criteria.getKey()), Boolean.parseBoolean(criteria.getValue().toString())));
+                                root.<String>get(criteria.getKey()), parseBoolean(criteria.getValue().toString())),
+                        predicates);
             } else if (root.get(criteria.getKey()).getJavaType() == Long.class) {
                 return criteriaBuilder.equal(
                         root.<String>get(criteria.getKey()), parseInt(criteria.getValue().toString()));
