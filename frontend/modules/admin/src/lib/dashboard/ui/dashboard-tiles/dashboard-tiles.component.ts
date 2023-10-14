7897cd8edf88c22202bab8b0d64b43cd96baf168
Adrian Kokot <kokotadriann@gmail.com>
2023-10-14 08:57:48 +0200

    feat: make dashbaord tiles appear after user data was retreived

--- frontend/modules/admin/src/lib/dashboard/ui/dashboard-tiles/dashboard-tiles.component.ts
+++ frontend/modules/admin/src/lib/dashboard/ui/dashboard-tiles/dashboard-tiles.component.ts
@@ -1,28 +1,30 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { TuiActionModule, TuiMarkerIconModule } from '@taiga-ui/kit';
-import { RouterLink } from '@angular/router';
-import { dashboardTiles } from '../../tiles';
-import { AuthService, isUserOfType } from '@reunice/modules/shared/security';
-import { TranslateModule } from '@ngx-translate/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TuiActionModule, TuiMarkerIconModule} from '@taiga-ui/kit';
+import {RouterLink} from '@angular/router';
+import {dashboardTiles} from '../../tiles';
+import {AuthService, isUserOfType} from '@reunice/modules/shared/security';
+import {TranslateModule} from '@ngx-translate/core';
+import {map} from "rxjs";
 
 @Component({
-  selector: 'reunice-dashboard-tiles',
-  standalone: true,
-  imports: [
-    CommonModule,
-    TuiActionModule,
-    RouterLink,
-    TuiMarkerIconModule,
-    TranslateModule,
-  ],
-  templateUrl: './dashboard-tiles.component.html',
-  styleUrls: ['./dashboard-tiles.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'reunice-dashboard-tiles',
+    standalone: true,
+    imports: [
+        CommonModule,
+        TuiActionModule,
+        RouterLink,
+        TuiMarkerIconModule,
+        TranslateModule,
+    ],
+    templateUrl: './dashboard-tiles.component.html',
+    styleUrls: ['./dashboard-tiles.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardTilesComponent {
-  private readonly _user = inject(AuthService).userSnapshot;
-  readonly links = dashboardTiles.filter(
-    (tile) => !tile.role || isUserOfType(this._user, tile.role),
-  );
+    readonly links$ = inject(AuthService).user$.pipe(
+        map(user => dashboardTiles.filter(
+            (tile) => !tile.role || isUserOfType(user, tile.role),
+        ))
+    );
 }
