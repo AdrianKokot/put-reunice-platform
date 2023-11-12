7de0eac500c5165a70ab6d36c4e83a013273f368
mudziok <mudziok@gmail.com>
2023-11-12 20:00:34 +0100

    Fix tests

--- frontend/src/app/ticket/ticket.component.spec.ts
+++ frontend/src/app/ticket/ticket.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketComponent } from './ticket.component';
+import { TestingModule } from '@reunice/testing';
 
 describe('TicketComponent', () => {
   let component: TicketComponent;
   let fixture: ComponentFixture<TicketComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TicketComponent],
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestingModule, TicketComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TicketComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
