0d97a63dbf6f84968b6f09701b618de8e44c3507
Wiktor <darkvikos@gmail.com>
2023-11-30 13:03:00 +0100

    feat: add translations

--- backend/src/main/java/com/example/cms/ticketUserStatus/exceptions/InvalidStatusChangeException.java
+++ backend/src/main/java/com/example/cms/ticketUserStatus/exceptions/InvalidStatusChangeException.java
@@ -3,5 +3,5 @@ package com.example.cms.ticketUserStatus.exceptions;
 import com.example.cms.validation.exceptions.BadRequestException;
 
 public class InvalidStatusChangeException extends BadRequestException {
-    public InvalidStatusChangeException () { super("ERROR.TICKET.400.INVALID_STATUS"); }
+    public InvalidStatusChangeException () { super("ERRORS.TICKET.400.INVALID_STATUS_CHANGE"); }
 }
