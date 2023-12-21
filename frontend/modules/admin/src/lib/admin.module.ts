a4b11f43e0594be2ed97049de4a8d369441d930b
Adrian Kokot <kokotadriann@gmail.com>
2023-12-21 08:44:12 +0100

    feat: add typography editor extension

--- frontend/modules/admin/src/lib/admin.module.ts
+++ frontend/modules/admin/src/lib/admin.module.ts
@@ -1,26 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { adminRoutes } from './lib.routes';
-import {
-  defaultEditorExtensions,
-  TUI_EDITOR_EXTENSIONS,
-} from '@tinkoff/tui-editor';
-import { provideDeactivateForm } from '@reunice/modules/shared/util';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {adminRoutes} from './lib.routes';
+import {defaultEditorExtensions, TUI_EDITOR_EXTENSIONS,} from '@tinkoff/tui-editor';
+import {provideDeactivateForm} from '@reunice/modules/shared/util';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(adminRoutes)],
-  providers: [
-    provideDeactivateForm(),
-    {
-      provide: TUI_EDITOR_EXTENSIONS,
-      useValue: [
-        ...defaultEditorExtensions,
-        import(
-          './shared/editor-extensions/load-template/load-template.extension'
-        ).then((m) => m.LoadTemplateExtension),
-      ],
-    },
-  ],
+    imports: [CommonModule, RouterModule.forChild(adminRoutes)],
+    providers: [
+        provideDeactivateForm(),
+        {
+            provide: TUI_EDITOR_EXTENSIONS,
+            useValue: [
+                ...defaultEditorExtensions,
+                import('@tiptap/extension-typography').then(m => m.Typography),
+                import(
+                    './shared/editor-extensions/load-template/load-template.extension'
+                    ).then((m) => m.LoadTemplateExtension),
+            ],
+        },
+    ],
 })
-export class AdminModule {}
+export class AdminModule {
+}
