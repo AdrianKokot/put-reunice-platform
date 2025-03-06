ea1b44ad86c78acc33540a25792b91e63c2a7c50
Adrian Kokot <kokotadriann@gmail.com>
2025-03-06 19:06:22 +0100

    feat(ui): add footer with placeholder images

--- frontend/modules/shared/ui/src/lib/ui.module.ts
+++ frontend/modules/shared/ui/src/lib/ui.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './layout/top-bar/top-bar.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { SearchComponent } from './layout/search/search.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule, TopBarComponent, SidebarComponent, SearchComponent],
+  imports: [CommonModule, TopBarComponent, SidebarComponent, SearchComponent, FooterComponent],
   declarations: [],
-  exports: [TopBarComponent, SidebarComponent, SearchComponent],
+  exports: [TopBarComponent, SidebarComponent, SearchComponent, FooterComponent],
 })
 export class UiModule {}
